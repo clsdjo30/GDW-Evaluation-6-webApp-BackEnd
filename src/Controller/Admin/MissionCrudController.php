0156740f8ae32b825b0282e41dc06798908342a7
Cedric Le Sergent <lesergent.cedric@gmail.com>
2022-07-15 18:19:55 +0200

    Add DateField and settime to configure crud

--- src/Controller/Admin/MissionCrudController.php
+++ src/Controller/Admin/MissionCrudController.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
@@ -29,6 +30,7 @@ class MissionCrudController extends AbstractCrudController
             ->setPageTitle('index', 'Toutes les %entity_label_plural%')
             ->setPageTitle('new', 'Ajouter une nouvelle %entity_label_singular%')
             ->setPaginatorPageSize(8)
+            ->setDateFormat('dd:MM:yyyy')
             ->showEntityActionsInlined()
             ->setDefaultSort(['id' => 'desc']);
 
@@ -44,6 +46,9 @@ class MissionCrudController extends AbstractCrudController
         yield TextField::new('code_name', 'Nom de Code')->setColumns(6);
 
         yield AssociationField::new('type', 'Type de mission')->setColumns(6);
+        yield DateField::new('startAt', 'Débute le:')
+            ->renderAsNativeWidget(false)
+            ->setColumns(3);
 
 
         yield AssociationField::new('country', 'Pays de mission');
