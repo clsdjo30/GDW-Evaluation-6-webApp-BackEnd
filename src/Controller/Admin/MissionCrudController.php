829e2e62f8a6c50f16adbff1d3e241a7d0212131
Cedric Le Sergent <lesergent.cedric@gmail.com>
2022-07-15 16:52:06 +0200

    Change hte DateTimeFields

--- src/Controller/Admin/MissionCrudController.php
+++ src/Controller/Admin/MissionCrudController.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
@@ -45,8 +45,12 @@ class MissionCrudController extends AbstractCrudController
         yield TextField::new('code_name', 'Nom de Code')->setColumns(6);
 
         yield AssociationField::new('type', 'Type de mission')->setColumns(6);
-        yield DateField::new('startAt', 'Débute le:')->setColumns(3);
-        yield DateField::new('endAt', 'Se termine: ')->setColumns(3);
+        yield DateTimeField::new('startAt', 'Débute le:')
+            ->renderAsText()
+            ->setColumns(3);
+        yield DateTimeField::new('endAt', 'Se termine: ')
+            ->renderAsText()
+            ->setColumns(3);
 
         yield AssociationField::new('country', 'Pays de mission');
 
