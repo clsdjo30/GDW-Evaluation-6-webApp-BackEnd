285d804346162822ec673144d63b75540c87a98d
Cedric Le Sergent <lesergent.cedric@gmail.com>
2022-07-16 09:10:33 +0200

    Customize Registration template

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -20,6 +20,7 @@ class RegistrationFormType extends AbstractType
             ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
+                'label' => 'Accepter ',
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
@@ -30,6 +31,7 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
+                'label' => 'Mot de passe',
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
